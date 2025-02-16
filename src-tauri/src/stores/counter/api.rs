fc8fe4bb560128ed8837e8ba15a54fac0caf7d75
Matthew Kwong <matttrv.kwong@gmail.com>
2025-02-16 13:33:51 -0800

    Update counterapi to use clearer default

--- src-tauri/src/stores/counter/api.rs
+++ src-tauri/src/stores/counter/api.rs
@@ -5,29 +5,32 @@ use taurpc;
 
 use super::types::CounterStore;
 
-// Default implementation for CounterApiImpl that sets the initial count to 0
-impl Default for CounterApiImpl {
-    fn default() -> Self {
-        Self::new(0)
-    }
-}
 
 // Define CounterApiImpl as a struct that contains a mutable CounterStore
 #[derive(Clone)]
 pub struct CounterApiImpl {
     // Must use a state key for thread safety
+    // and wrap the store state type in an Arc<Mutex<>>
     state: Arc<Mutex<CounterStore>>,
 }
 
-// Constructor for CounterApiImpl
-impl CounterApiImpl {
-    pub fn new(initial_count: i32) -> Self {
-        let store = CounterStore {
-            count: initial_count,
+// Default implementation for CounterApiImpl that sets the initial count to 0
+impl Default for CounterApiImpl {
+    fn default() -> Self {
+        let initial_state = CounterStore {
+            count: 0,
         };
 
+        Self::new(initial_state)
+    }
+}
+
+
+// Constructor for CounterApiImpl
+impl CounterApiImpl {
+    pub fn new(initial_state: CounterStore) -> Self {
         Self { 
-            state: Arc::new(Mutex::new(store)),
+            state: Arc::new(Mutex::new(initial_state)),
         }
     }
     // Helper method to emit state changes
@@ -42,6 +45,7 @@ impl CounterApiImpl {
     where
         F: FnOnce(&mut CounterStore)
     {
+        // Lock the variable to modify
         let mut state = self.state.lock().await;
 
         // Updater is generic function that can modify the state
@@ -59,37 +63,23 @@ impl CounterApiImpl {
 )]
 pub trait CounterApi {
     async fn increase(app_handle: AppHandle<Wry>) -> Result<(), String>;
-    async fn get_data() -> CounterStore;
 
-    #[taurpc(event)] // Define the data_changed method as an event
+    async fn get_data() -> CounterStore;
+    #[taurpc(event)] // Define the on_updated method as an event for frontend to listen to
     async fn on_updated(new_data: CounterStore);
 }
 
 // Define the resolvers for the CounterApi trait
 #[taurpc::resolvers]
 impl CounterApi for CounterApiImpl {
+    // implement increase method
     async fn increase(self, app_handle: AppHandle<Wry>) -> Result<(), String> {
+        // wrap the 
         self.update_state(app_handle, |state| {
             state.count += 1;
             println!("Count increased: {}", state.count);
         }).await
     }
-
-    // async fn increase(self, app_handle: AppHandle<Wry>) -> Result<(), String> {
-    //     // Lock the state and increment the count
-    //     // must await since mutex is async
-    //     let mut state = self.state.lock().await;
-    //     state.count += 1;
-    //     println!("Count increased: {}", state.count);
-        
-    //     // Trigger the on_updated event to send to frontend
-    //     // passing the new state to the event
-    //     CounterEventTrigger::new(app_handle)
-    //         .on_updated(CounterStore {
-    //             count: state.count,
-    //         })
-    //         .map_err(|e| e.to_string())
-    // }
 
     // Get the current state of the counter
     async fn get_data(self) -> CounterStore {
