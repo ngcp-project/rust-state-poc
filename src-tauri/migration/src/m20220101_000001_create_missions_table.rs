8c32344fc881aa2b565b5cefc8e0e5c91aef709a
Michael Wu <mwu250@gmail.com>
2025-03-24 17:20:50 -0700

    fix: create mission table

--- src-tauri/migration/src/m20220101_000001_create_missions_table.rs
+++ src-tauri/migration/src/m20220101_000001_create_missions_table.rs
@@ -1,44 +1,62 @@
 // m20220101_000001_create_missions_table.rs
 use sea_orm_migration::prelude::*;
+use sea_orm::{EnumIter, Iterable};
+use sea_orm_migration::prelude::extension::postgres::Type;
 
 pub struct Migration;
 
 impl MigrationName for Migration {
-    fn name(&self) -> &str {
-        "m20220101_000001_create_missions_table" // Make sure this matches with the file name
-    }
+  fn name(&self) -> &str {
+    "m20220101_000001_create_missions_table" // Make sure this matches with the file name
+  }
+}
+
+#[derive(Iden, EnumIter)]
+pub enum Statuses {
+  #[iden = "Active"]
+  Active,
+  #[iden = "Inactive"]
+  Inactive,
+  #[iden = "Complete"]
+  Complete,
+  #[iden = "Failed"]
+  Failed,
 }
 
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
-    // Define how to apply this migration: Create the Bakery table.
-    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        manager
-            .create_table(
-                Table::create()
-                    .table(Missions::Table)
-                    .col(ColumnDef::new(Missions::Name).string().not_null().primary_key())
-                    .col(ColumnDef::new(Missions::KeepInZones).array(ColumnType::String(32)).not_null())
-                    .col(ColumnDef::new(Missions::KeepOutZones).array(ColumnType::String(Some(32))).not_null())
-                    .col(ColumnDef::new(Missions::Status).enumeration("mission_status", &["Active", "Inactive", "Complete", "Failed"]).not_null())
-                    .to_owned(),
-            )
-            .await
-    }
+  // Define how to apply this migration: Create the Bakery table.
+  async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+    manager
+    .create_type(
+        Type::create()
+            .as_enum(Alias::new("status"))
+            .values([Alias::new("Active"), Alias::new("Inactive"), Alias::new("Complete"), Alias::new("Failed")])
+            .to_owned(),
+    )
+    .await?;
+    manager.create_table(
+      Table::create()
+        .table(Missions::Table)
+        .col(ColumnDef::new(Missions::Name).string().not_null().primary_key())
+        .col(ColumnDef::new(Missions::KeepInZones).array(ColumnType::Text).not_null())
+        .col(ColumnDef::new(Missions::KeepOutZones).array(ColumnType::Text).not_null())
+        .col(ColumnDef::new(Missions::Status).enumeration(Alias::new("status"), Statuses::iter()))
+        .to_owned()
+    ).await
+  }
 
-    // Define how to rollback this migration: Drop the Missions table.
-    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        manager
-            .drop_table(Table::drop().table(Missions::Table).to_owned())
-            .await
-    }
+  // Define how to rollback this migration: Drop the Missions table.
+  async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+    manager.drop_table(Table::drop().table(Missions::Table).to_owned()).await
+  }
 }
 
 #[derive(Iden)]
 pub enum Missions {
-    Table,
-    Name,
-    KeepInZones,
-    KeepOutZones,
-    Status,
+  Table,
+  Name,
+  KeepInZones,
+  KeepOutZones,
+  Status,
 }
